bda506141ccd37a475b0a5c5f2ecf469c0aaf3b2
pauloalarico <paulocraft20@gmail.com>
2026-01-30 22:59:45 -0300

    refactor(Product-Service) removing methods never used

--- src/main/java/org/example/productmicrosservice/appllication/service/ProductService.java
+++ src/main/java/org/example/productmicrosservice/appllication/service/ProductService.java
@@ -15,35 +15,14 @@ public class ProductService {
 
     private final ProductRepository repository;
 
-
-    public Product getProductById(String id) {
-       return findProductById(id);
-    }
-
     public Product registerNewProduct(RegisterProductDto dto) {
         var product = new Product(dto);
         return repository.save(product);
     }
 
-    @Transactional
-    public Product decreaseStockQuantity(String id, Integer quantity) {
-        var product = findProductById(id);
-        if(quantity > product.getStockQuantity()) {
-            throw new RuntimeException("Order with more quantity to product than the stock");
-        }
-        repository.updateStockByQuantity(UUID.fromString(id), quantity);
-        return product;
-    }
 
     public Product findProductById(String id) {
         return repository.findByUid(UUID.fromString(id))
                 .orElseThrow(() -> new RuntimeException("Product Id invalid or do not exist!"));
-    }
-
-    @Transactional
-    public Product resetStockByQuantity(String id, Integer quantity) {
-        var product = findProductById(id);
-        repository.reset(UUID.fromString(id), quantity);
-        return product;
     }
 }
